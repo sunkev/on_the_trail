a7396572d639c9ce053b96b24fcfa826d32b7bee
Kevin Sun <sunkev@brandeis.edu>
2016-07-05 22:06:10 -0400

    expands notification spec

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -6,13 +6,30 @@ describe Notification do
   it { should validate_presence_of(:contact_email) }
   it { should validate_presence_of(:message) }
 
-  it 'has different attributes' do
-    notification = Notification.create(first_name: 'John', last_name: 'smith',
-      contact_email: 'john@john.com', message: 'AHhh!')
+  let(:notification) { Notification.create(first_name: 'John', last_name: 'smith',
+    contact_email: 'john@john.com', message: 'AHhh!', latitude: 30, longitude: 45)}
 
+  it 'has a first name' do
     expect(notification.first_name).to eql 'John'
+  end
+
+  it 'has a last name' do
     expect(notification.last_name).to eql 'smith'
+  end
+
+  it 'has a contact email' do
     expect(notification.contact_email).to eql 'john@john.com'
+  end
+
+  it 'has a message' do
     expect(notification.message).to eql 'AHhh!'
+  end
+
+  it 'has a latitude' do
+    expect(notification.latitude).to eql 30.0
+  end
+
+  it 'has a longitude' do
+    expect(notification.longitude).to eql 45.0
   end
 end
